55d895f6f358b0363fa232fc2383754c216e38a0
Colin Poon <me@colinpoon.com>
2021-09-13 20:01:09 +0000

    all Head.js parameters complete

---
+++ head.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(arr) {
+  if (arr === []) {
+    return undefined;
+  }
+  return arr[0];
+};
+//TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5,6,7]), 6);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
+assertEqual(head([]), undefined);
