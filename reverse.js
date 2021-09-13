152b3261505d7b5eb4363d6f61ea820cc9433fd3
Colin Poon <me@colinpoon.com>
2021-09-13 20:51:02 +0000

    removed and moved reverse.js

--- reverse.js
+++
@@ -1,9 +0,0 @@
-function reverse(str) {
-  let output = '';
-  for(let i = str.length-1; i >= 0 ; i--) {
-    output = output + str[i];
-  }
-  return output;
-};
-console.log(reverse('LIGHTHOUSE'));
-console.log(reverse('NAME'));
