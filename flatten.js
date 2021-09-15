6762e35ecd6d713a97fe21c993b423d48569098d
Colin Poon <me@colinpoon.com>
2021-09-15 04:00:59 +0000

    flatten stretch

---
+++ flatten.js
@@ -0,0 +1,29 @@
+const eqArrays = function(arrA, arrB) {
+  if (arrA.length !== arrB.length) {
+    return false;
+  }
+  for (let i = 0; i < arrA.length; i++) {
+    if (arrA[i] !== arrB[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+// eqArrays
+
+const assertArraysEqual = function (arrA, arrB) {
+  if(eqArrays(arrA, arrB)) {
+    console.log(`✅ Arrays Passed: ${arrA} = ${arrB}`);
+  } else {
+    console.log(`💥 Arrays Failed: ${arrA} ≠ ${arrB}`);
+  }
+};
+//assertArraysEqual
+
+const flatten = function(arr) {
+  return arr.flat();
+};
+
+
+//TEST CODE
+console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
