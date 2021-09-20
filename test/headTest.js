4626e9ef9084b42c8ab87c1d0b7324fdc1551810
Colin Poon <me@colinpoon.com>
2021-09-20 19:39:21 +0000

    mocha & chai assert libraries added to head.js && functional

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,20 @@
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-//TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5,6,7]), 6);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
-assertEqual(head([]), undefined);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 'undefined' for empty array []", () => {
+    assert.strictEqual(head([]), undefined); 
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello')
+  });
+});
